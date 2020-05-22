6c851789341e1ea9fc40256a3f98095565a0dbe2
YangTingPyQt5 <59390374+YangTingPyQt5@users.noreply.github.com>
2020-05-22 18:34:51 +0800

    SAR66 M2 Quiz

---
+++ highscores/highscores.js
@@ -0,0 +1,15 @@
+const highScoresList = document.getElementById("highScoresList");
+const topicScore = document.getElementById("topicScore");
+const recentScore = document.getElementById("recentScore");
+var mostRecentScore = localStorage.getItem("mostRecentScore");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const topicScoreTable = JSON.parse(localStorage.getItem("topicScore")) || [];
+
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class="high-score">${score.name}: ${score.score} - ${score.time}</li>`;
+  }).join("");
+topicScore.innerHTML = topicScoreTable.map(score =>{
+  return `<tr><td>${score.topicName}:</td><td class="topic-score">${score.correct} / ${score.numQ}</td><td>${score.status}</td></tr>`
+}).join("");
+
+recentScore.innerText = (Number(mostRecentScore)/52*100).toPrecision(3) + "%";

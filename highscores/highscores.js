c3a978045efe606190f871b8989f6ccd4bdf05d1
YangTingPyQt5 <59390374+YangTingPyQt5@users.noreply.github.com>
2020-05-27 16:21:52 +0800

    Adding m3.html page

--- highscores/highscores.js
+++ highscores/highscores.js
@@ -8,11 +8,5 @@ const topicScoreTable = JSON.parse(localStorage.getItem("topicScore")) || [];
 highScoresList.innerHTML = highScores.map(score => {
     return `<li class="high-score">${score.name}: ${score.score} - ${score.time}</li>`;
   }).join("");
-topicScore.innerHTML = topicScoreTable.map(score =>{
-  return `<tr><td class="topic-score">${score.topicName}:</td>
-              <td class="topic-score">${score.correct} / ${score.numQ}</td>
-              <td class="topic-score">${score.status}</td>
-          </tr>`
-}).join("");
 
 recentScore.innerText = (Number(mostRecentScore)/52*100).toPrecision(3) + "%";
